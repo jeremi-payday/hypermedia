e2dc9c64413508e14b59292516cb6482a9664a55
Alexandre Barone <200929430@collegeahuntsic.qc.ca>
2016-11-03 22:36:40 -0400

    Removed unused commented code.

--- src/java/main/User.java
+++ src/java/main/User.java
@@ -91,44 +91,4 @@ public class User {
     public boolean IsAdmin(){
         return this.admin > 0;
     }
-    /*
-    private int id;
-    private String username;
-    private String password;
-    private int admin;
-    
-    public User(String username, String password) {
-        this.password = password;
-        this.username = username;
-        this.admin = 0;
-    }
-    public User(int id, String username, int isAdmin) {
-        this.id = id;
-        this.username = username;
-        this.admin = isAdmin;
-    }
-    public User(int id, String username, String password, int isAdmin) {
-        this(id, username, isAdmin);
-        this.password = password;
-        this.admin = isAdmin;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    
-    public String getPassword(){
-        return this.password;
-    }
-    public boolean IsAdmin(){
-        return this.admin > 0;
-    }*/
 }
